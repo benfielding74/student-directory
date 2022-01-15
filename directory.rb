2631fe63e90f119bc031b67c6a36543a45df4c01
benfielding74 <billy.fadden@live.com>
2022-01-15 15:51:08 +0000

    Changed the student list to an array of hashes and refactored

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex Delarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :january},
+  {name: "Darth Vader", cohort: :january},
+  {name: "Nurse Ratched", cohort: :january},
+  {name: "Michael Corleone", cohort: :january},
+  {name: "Alex Delarge", cohort: :january},
+  {name: "The Wicked Witch of the West", cohort: :january},
+  {name: "Terminator", cohort: :january},
+  {name: "Freddy Krueger", cohort: :january},
+  {name: "The Joker", cohort: :january},
+  {name: "Joffrey Baratheon", cohort: :january},
+  {name: "Norman Bates", cohort: :january}
 ]
 
 def print_header
@@ -17,8 +17,8 @@ def print_header
   puts "_ _ _ _ _ _ _ _ _ _ _ _ _"
 end
 
-def print(names)
-  names.each { |name| puts name }
+def print(students)
+  students.each { |name| puts "#{name[:name]} (#{name[:cohort]} cohort)" }
 end
 
 def print_footer(names)
